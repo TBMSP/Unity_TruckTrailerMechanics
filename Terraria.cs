8877e4dea5bd94dbd81675495d82ed11d4d08e52
TBM SP <soporte@tbmsp.net>
2020-04-02 14:09:47 -0300

    Add files via upload

---
+++ Terraria.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Terraria:MonoBehaviour{
+private Transform[] childs;
+void Start(){
+childs=new Transform[transform.childCount];
+for(int i=0;i<childs.Length;i++){
+childs[i]=transform.GetChild(i);
+}
+}
+void Update(){
+for(int i=1;i<childs.Length;i++){
+var a=childs[i-1];
+var b=childs[i];
+var c=0.75f;
+b.position=(b.position-a.position).normalized*(c+3)+a.position;
+b.LookAt(a.position);
+}
+}
+}
---
+++ Truck.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Truck:MonoBehaviour{
+public Transform pivotTruck,trailer;
+public float trailerDistance;
+void Update(){
+trailer.position=(trailer.position-pivotTruck.position).normalized*trailerDistance+pivotTruck.position;
+trailer.LookAt(pivotTruck.position);
+}
+}
